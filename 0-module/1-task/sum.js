76933cb93da4aa3e33b6a6cf35cee68902678917
Pavel Mikheev <pavel.mikheev@t-systems.com>
2019-11-29 13:43:14 +0300

    right sol

--- 0-module/1-task/sum.js
+++ 0-module/1-task/sum.js
@@ -1,9 +1,9 @@
 function sum(a, b) {
-  if (typeof a === 'number' && typeof b === 'number') {
-    return a + b;
-  } else {
+  if ([a, b].some((value) => typeof value !== 'number')) {
     throw new TypeError();
   }
+
+  return a + b;
 }
 
 module.exports = sum;
